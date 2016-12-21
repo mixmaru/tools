54d6028f79ba2713595b45ad6c326f0c845a4118
maru <mix.touch777@gmail.com>
2016-12-21 21:39:39 +0900

    コマンド実行時の引数を解析して実行関数を実行するところまで行った

--- cd_work_dir.bash
+++ cd_work_dir.bash
@@ -1,18 +1,79 @@
 #!/bin/bash
 
-cd `dirname $0`
+#ロックファイルの存在確認
+#ロックファイルの作成
 
-#path="$(cat ./.cdwkdir | awk '{print $2}')"
-#echo ${path} > ./out.txt
 
-project_name=$1
-echo $1
+#使用するファイルのパスを用意
+#work_dir_path=スクリプトがあるディレクトリの/.cdwkdirディレクトリ
+#setting_file=${work_dir_path}config
+#lock_file=${work_dir_path}.lock
 
-count_command="cat ./.cdwkdir | awk 'BEGIN{count=0} \$1==\"${project_name}\"{count+=1} END{print count}'"
-count=$(eval ${count_command})
+#関数の定義
+function getopt(){
+    echo "getopt"
+    echo $1
+}
+function move(){
+    echo "move"
+    echo $1
+}
+function add(){
+    echo "add"
+    echo $1
+}
+function modify(){
+    echo "modify"
+    echo $1
+}
+function delete(){
+    echo "delete"
+    echo $1
+}
 
-if [ ${count} == 1 ]; then
-    echo "ok"
-else
-    echo "ng"
+mode="unset"
+#処理モードとプロジェクト名を取得
+if [ $# = 1 ]; then
+    mode="move"
+    project=$1
+elif [ $# = 2 ]; then
+    case "$1" in
+        "add"       )
+            mode="add"
+            project=$2
+            ;;
+        "modify"    )
+            mode="modify"
+            project=$2
+            ;;
+        "delete"    )
+            mode="delete"
+            project=$2
+            ;;
+    esac
 fi
+
+if [ "${mode}" = "unset" ];then
+    echo "引数を正しく指定してください"
+    exit 0;
+fi
+
+case "${mode}" in
+  "move"    ) move ${project};;
+  "add"     ) add ${project};;
+  "modify"  ) modify ${project};;
+  "delete"  ) delete ${project};;
+esac
+
+
+
+#project_name=$1
+#
+#count_command="cat ./.cdwkdir | awk 'BEGIN{count=0} \$1==\"${project_name}\"{count+=1} END{print count}'"
+#count=$(eval ${count_command})
+#
+#if [ ${count} == 1 ]; then
+#    echo "ok"
+#else
+#    echo "ng"
+#fi

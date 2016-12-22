4205cad20316c830c161269794ec4d5c94b6d4af
maru <mix.touch777@gmail.com>
2016-12-22 21:38:12 +0900

    排他ロックをかけた

--- cd_work_dir.bash
+++ cd_work_dir.bash
@@ -5,9 +5,24 @@
 
 
 #使用するファイルのパスを用意
-#work_dir_path=スクリプトがあるディレクトリの/.cdwkdirディレクトリ
-#setting_file=${work_dir_path}config
-#lock_file=${work_dir_path}.lock
+#スクリプトがあるディレクトリ
+script_dir=$(cd $(dirname $0) && pwd)/
+#ロック用ファイル
+lock_file=${script_dir}.cdwkdirlock
+#設定ファイルディレクトリ
+work_dir=${script_dir}.cdwkdir/
+#設定ファイル
+setting_file=${work_dir}config
+
+
+#排他ロック。参考）http://qiita.com/hidetzu/items/11f92f941efbb182f757
+is_lock="no"
+ln -s $0 ${lock_file} 2> /dev/null || is_lock="yes"
+if [ ${is_lock} = "yes" ]; then
+    echo "多重起動防止" 1>&2
+    exit 1
+fi
+
 
 #関数の定義
 function getopt(){
@@ -31,8 +46,9 @@ function delete(){
     echo $1
 }
 
-mode="unset"
+
 #処理モードとプロジェクト名を取得
+mode="unset"
 if [ $# = 1 ]; then
     mode="move"
     project=$1
@@ -53,12 +69,21 @@ elif [ $# = 2 ]; then
     esac
 fi
 
+#引数が正しく指定されなかった場合
 if [ "${mode}" = "unset" ];then
     echo "引数を正しく指定してください"
+    #ロック開放
+    rm -f ${lock_file}
     exit 0;
 fi
 
+
+#メイン処理実行
 ${mode} ${project}
+
+
+#ロック開放
+rm -f ${lock_file}
 
 #project_name=$1
 #

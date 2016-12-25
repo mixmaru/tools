2d21403549197e01fe71c4b4da04de431ed3c281
maru <mix.touch777@gmail.com>
2016-12-25 14:57:39 +0900

    パス変数を定数チックにした。readonlyはunsetできないので使わない

--- cd_work_dir.bash
+++ cd_work_dir.bash
@@ -5,16 +5,16 @@
 
 #使用するファイルのパスを用意
 #スクリプトがあるディレクトリ
-script_dir=$(cd $(dirname $1) && pwd)/
-script_file=${script_dir}cd_work_dir.bash
+SCRIPT_DIR=$(cd $(dirname $1) && pwd)/
+SCRIPT_FILE=${SCRIPT_DIR}cd_work_dir.bash
 #ロック用ファイル
-lock_file=${script_dir}.cdwkdirlock
+LOCK_FILE=${SCRIPT_DIR}.cdwkdirlock
 #設定ファイルディレクトリ
-work_dir=${script_dir}.cdwkdir/
+WORK_DIR=${SCRIPT_DIR}.cdwkdir/
 #設定ファイル
-setting_file=${work_dir}config
+PROJECT_LIST_FILE=${WORK_DIR}project_list
 #一時保存ファイル
-setting_tmp_file=${work_dir}config_tmp
+PROJECT_TMP_FILE=${WORK_DIR}.project_list_tmp
 
 #使用する変数
 mode=
@@ -25,7 +25,7 @@ error=
 #排他ロック。参考）http://qiita.com/hidetzu/items/11f92f941efbb182f757
 function lock(){
     local is_lock="no"
-    ln -s $1 ${lock_file} 2> /dev/null || is_lock="yes"
+    ln -s $1 ${LOCK_FILE} 2> /dev/null || is_lock="yes"
     if [ ${is_lock} = "yes" ]; then
         return 1
     else
@@ -37,7 +37,7 @@ function lock(){
 function move(){
     local project_name=$1
     #指定プロジェクトの行数を取得
-    local count_command="cat ${setting_file} | awk 'BEGIN{count=0} \$1==\"${project_name}\"{count+=1} END{print count}'"
+    local count_command="cat ${PROJECT_LIST_FILE} | awk 'BEGIN{count=0} \$1==\"${project_name}\"{count+=1} END{print count}'"
     local count=$(eval ${count_command})
 
     #行数が1なら正常。そうでないなら異常なので処理終了
@@ -45,12 +45,12 @@ function move(){
         error="${project_name}の設定は存在しません"
         return 1
     elif [ ${count} -ge 2 ]; then
-        error="${project_name}の設定が複数あります。${setting_file} を確認してください"
+        error="${project_name}の設定が複数あります。${PROJECT_LIST_FILE} を確認してください"
         return 1
     fi
 
     #パスを取り出す
-    local path_command="cat ${setting_file} | awk '\$1==\"${project_name}\"{print \$2}'"
+    local path_command="cat ${PROJECT_LIST_FILE} | awk '\$1==\"${project_name}\"{print \$2}'"
     local target_path=$(eval ${path_command})
     cd ${target_path}
 }
@@ -60,11 +60,11 @@ function add(){
     local name=$1
     local path=$2
     #存在しないかチェック
-    local tmp_command="cat ${setting_file} | awk '\$1==\"${name}\"{print \$1}'"
+    local tmp_command="cat ${PROJECT_LIST_FILE} | awk '\$1==\"${name}\"{print \$1}'"
     local result=($(eval ${tmp_command}))
     if [ ${#result[@]} -eq 0 ];then
         #追加処理
-        echo "${name} ${path}" >> ${setting_file}
+        echo "${name} ${path}" >> ${PROJECT_LIST_FILE}
         return 0
     else
         error="すでに${name}が存在します"
@@ -75,20 +75,21 @@ function add(){
 #プロジェクト削除 delete プロジェクト名
 function delete(){
     local name=$1
-    local tmp_command="cat ${setting_file} | awk '\$1!=\"${name}\"{print \$0}'"
+    local tmp_command="cat ${PROJECT_LIST_FILE} | awk '\$1!=\"${name}\"{print \$0}'"
 
     #指定プロジェクトを除去した設定ファイルの一時ファイルを作成
-    eval ${tmp_command} > ${setting_tmp_file}
+    eval ${tmp_command} > ${PROJECT_TMP_FILE}
     #元ファイルと比較して、変更があれば元ファイルを上書きする
-    local tmp_command="diff ${setting_file} ${setting_tmp_file}"
+    local tmp_command="diff ${PROJECT_LIST_FILE} ${PROJECT_TMP_FILE}"
     local diff_res=$(eval ${tmp_command})
 
     if [ -n "${diff_res}" ];then
-        cat ${setting_tmp_file} > ${setting_file}
-        rm ${setting_tmp_file}
+        cat ${PROJECT_TMP_FILE} > ${PROJECT_LIST_FILE}
+        rm ${PROJECT_TMP_FILE}
         return 0
     else
         error="${name}は存在しません"
+        rm ${PROJECT_TMP_FILE}
         return 1
     fi
 }
@@ -121,14 +122,14 @@ function getOption(){
 }
 
 #排他ロック
-lock ${script_file}
+lock ${SCRIPT_FILE}
 
 #設定ファイル等がなければ作成する
-if [ ! -d  ${work_dir} ];then
-    mkdir ${work_dir}
+if [ ! -d  ${WORK_DIR} ];then
+    mkdir ${WORK_DIR}
 fi
-if [ ! -e ${setting_file} ]; then
-    touch ${setting_file}
+if [ ! -e ${PROJECT_LIST_FILE} ]; then
+    touch ${PROJECT_LIST_FILE}
 fi
 
 if [ $? -eq 0 ]; then
@@ -145,84 +146,7 @@ if [ $? -eq 0 ]; then
     fi
 
     #ロック開放
-    rm -f ${lock_file}
+    rm -f ${LOCK_FILE}
 else
     echo "多重起動防止"
 fi
-
-
-
-
-#関数の定義
-#function move(){
-#    local project_name=$1
-#    #指定プロジェクトの行数を取得
-#    local count_command="cat ${setting_file} | awk 'BEGIN{count=0} \$1==\"${project_name}\"{count+=1} END{print count}'"
-#    local count=$(eval ${count_command})
-#
-#    #行数が1なら正常。そうでないなら異常なので処理終了
-#    if [ ${count} -eq 0 ]; then
-#        errorExit "${project_name}の設定は存在しません"
-#    elif [ ${count} -ge 2 ]; then
-#        errorExit "${project_name}の設定が複数あります。${setting_file} を確認してください"
-#    fi
-#
-#    #パスを取り出す
-#    local path_command="cat ${setting_file} | awk '\$1==\"${project_name}\"{print \$2}'"
-#    local target_path=$(eval ${path_command})
-#    cd ${target_path}
-#}
-#function add(){
-#    echo "add"
-#    echo $1
-#}
-#function modify(){
-#    echo "modify"
-#    echo $1
-#}
-#function delete(){
-#    echo "delete"
-#    echo $1
-#}
-#function errorExit(){
-#    echo $1
-#    #ロック開放
-#    rm -f ${lock_file}
-#    exit 0;
-#}
-#
-##処理モードとプロジェクト名を取得
-#mode="unset"
-#if [ $# = 2 ]; then
-#    mode="move"
-#    project=$2
-#elif [ $# = 3 ]; then
-#    case "$1" in
-#        "add"       )
-#            mode="add"
-#            project=$3
-#            ;;
-#        "modify"    )
-#            mode="modify"
-#            project=$3
-#            ;;
-#        "delete"    )
-#            mode="delete"
-#            project=$3
-#            ;;
-#    esac
-#fi
-#
-##引数が正しく指定されなかった場合
-#if [ "${mode}" = "unset" ];then
-#    errorExit "引数を正しく指定してください"
-#fi
-#
-#
-##メイン処理実行
-#${mode} ${project}
-#
-#
-##ロック開放
-#rm -f ${lock_file}
-

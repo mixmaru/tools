13d80527cf44f928726025c163d5c935ee7c3a82
maru <mix.touch777@gmail.com>
2016-12-25 15:39:34 +0900

    主要な変数はグローバル変数を使うようにした

--- cd_work_dir.bash
+++ cd_work_dir.bash
@@ -12,6 +12,10 @@ TMP_CD_WORK_DIR_LOCK_FILE=${LOCK_FILE}
 TMP_CD_WORK_DIR_WORK_DIR=${WORK_DIR}
 TMP_CD_WORK_DIR_PROJECT_LIST_FILE=${PROJECT_LIST_FILE}
 TMP_CD_WORK_DIR_PROJECT_TMP_FILE=${PROJECT_TMP_FILE}
+TMP_CD_WORK_DIR_mode=${mode}
+TMP_CD_WORK_DIR_project_name=${project_name}
+TMP_CD_WORK_DIR_project_path=${project_path}
+TMP_CD_WORK_DIR_error=${error}
 
 #使用するファイルのパスを用意
 #スクリプトがあるディレクトリ
@@ -28,14 +32,15 @@ PROJECT_TMP_FILE=${WORK_DIR}.project_list_tmp
 
 #使用する変数
 mode=
-project=
+project_name=
+project_path=
 error=
 
 #関数の定義
 #排他ロック。参考）http://qiita.com/hidetzu/items/11f92f941efbb182f757
 function lock(){
     local is_lock="no"
-    ln -s $1 ${LOCK_FILE} 2> /dev/null || is_lock="yes"
+    ln -s ${SCRIPT_FILE} ${LOCK_FILE} 2> /dev/null || is_lock="yes"
     if [ ${is_lock} = "yes" ]; then
         return 1
     else
@@ -45,7 +50,6 @@ function lock(){
 
 #カレントディレクトリの移動
 function move(){
-    local project_name=$1
     #指定プロジェクトの行数を取得
     local count_command="cat ${PROJECT_LIST_FILE} | awk 'BEGIN{count=0} \$1==\"${project_name}\"{count+=1} END{print count}'"
     local count=$(eval ${count_command})
@@ -65,27 +69,24 @@ function move(){
     cd ${target_path}
 }
 
-#プロジェクトパスデータの追加 add 追加プロジェクト名 パス
+#プロジェクトパスデータの追加
 function add(){
-    local name=$1
-    local path=$2
     #存在しないかチェック
-    local tmp_command="cat ${PROJECT_LIST_FILE} | awk '\$1==\"${name}\"{print \$1}'"
+    local tmp_command="cat ${PROJECT_LIST_FILE} | awk '\$1==\"${project_name}\"{print \$1}'"
     local result=($(eval ${tmp_command}))
     if [ ${#result[@]} -eq 0 ];then
         #追加処理
-        echo "${name} ${path}" >> ${PROJECT_LIST_FILE}
+        echo "${project_name} ${project_path}" >> ${PROJECT_LIST_FILE}
         return 0
     else
-        error="すでに${name}が存在します"
+        error="すでに${project_name}が存在します"
         return 1
     fi
 }
 
 #プロジェクト削除 delete プロジェクト名
 function delete(){
-    local name=$1
-    local tmp_command="cat ${PROJECT_LIST_FILE} | awk '\$1!=\"${name}\"{print \$0}'"
+    local tmp_command="cat ${PROJECT_LIST_FILE} | awk '\$1!=\"${project_name}\"{print \$0}'"
 
     #指定プロジェクトを除去した設定ファイルの一時ファイルを作成
     eval ${tmp_command} > ${PROJECT_TMP_FILE}
@@ -98,7 +99,7 @@ function delete(){
         rm ${PROJECT_TMP_FILE}
         return 0
     else
-        error="${name}は存在しません"
+        error="${project_name}は存在しません"
         rm ${PROJECT_TMP_FILE}
         return 1
     fi
@@ -108,19 +109,19 @@ function delete(){
 function getOption(){
     if [ -n "$1" ] && [ -z "$2" ]; then
         mode="move"
-        project=$1
+        project_name=$1
     elif [ -n "$1" ] && [ -n "$2" ]; then
         case "$1" in
             "add"       )
                 if [ -n "$3" ];then
                     mode="add"
-                    project=$2
+                    project_name=$2
                     project_path=$3
                 fi
                 ;;
             "delete"    )
                 mode="delete"
-                project=$2
+                project_name=$2
                 ;;
         esac
     fi
@@ -132,7 +133,7 @@ function getOption(){
 }
 
 #排他ロック
-lock ${SCRIPT_FILE}
+lock
 if [ $? -eq 0 ]; then
     #設定ファイル等がなければ作成する
     if [ ! -d  ${WORK_DIR} ];then
@@ -146,7 +147,7 @@ if [ $? -eq 0 ]; then
     getOption $2 $3 $4
     if [ $? -eq 0 ];then
         #メイン処理実行
-       ${mode} ${project} ${project_path}
+       ${mode}
     fi
 
     #エラーがあればメッセージを出す
@@ -167,3 +168,7 @@ LOCK_FILE=${TMP_CD_WORK_DIR_LOCK_FILE}
 WORK_DIR=${TMP_CD_WORK_DIR_WORK_DIR}
 PROJECT_LIST_FILE=${TMP_CD_WORK_DIR_PROJECT_LIST_FILE}
 PROJECT_TMP_FILE=${TMP_CD_WORK_DIR_PROJECT_TMP_FILE}
+mode=${TMP_CD_WORK_DIR_mode}
+project_name=${TMP_CD_WORK_DIR_project_name}
+project_path=${TMP_CD_WORK_DIR_project_path}
+error=${TMP_CD_WORK_DIR_error}
